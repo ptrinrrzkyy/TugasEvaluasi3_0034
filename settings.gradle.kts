a16020a71e5b2cff4a9d3b762ddcea65c4888039
Putri Nur Rezky <133496292+ptrinrrzkyy@users.noreply.github.com>
2024-04-26 15:32:47 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "B1_Prak3-13120220034"
+include(":app")
+ 
